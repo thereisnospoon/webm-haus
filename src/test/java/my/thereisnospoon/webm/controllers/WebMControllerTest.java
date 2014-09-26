f1017aaa04c7d170e54b6f82ffd05e168cc8dcaa
thereisnospoon <ocheyoba@gmail.com>
2014-09-26 18:22:41 +0300

    Moved back location of spring config file

--- src/test/java/my/thereisnospoon/webm/controllers/WebMControllerTest.java
+++ src/test/java/my/thereisnospoon/webm/controllers/WebMControllerTest.java
@@ -2,17 +2,12 @@ package my.thereisnospoon.webm.controllers;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.runners.MockitoJUnitRunner;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
-import static org.mockito.Mockito.*;
-
-import javax.servlet.http.HttpServletRequest;
-
-@ContextConfiguration(locations = {"file:src/main/java/my/thereisnospoon/webm/webm-config.xml"})
+@ContextConfiguration(locations = {"file:src/main/webapp/WEB-INF/webm-config.xml"})
 @WebAppConfiguration
 @RunWith(SpringJUnit4ClassRunner.class)
 public class WebMControllerTest {
