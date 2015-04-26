15574e5e7eca41a858a350434ea9dbece5d7223d
unknown <vindimon@gmail.com>
2015-04-26 11:30:31 +0300

    test commit

--- src/test/java/my/thereisnospoon/webm/Test.java
+++ src/test/java/my/thereisnospoon/webm/Test.java
@@ -1,4 +1,16 @@
 package my.thereisnospoon.webm;
 
+import java.util.*;
+
 public class Test {
+
+	private Object o = new Object();
+
+	@org.junit.Test
+	public void test() throws Exception {
+
+		synchronized (o) {
+			o.wait(2000);
+		}
+	}
 }
