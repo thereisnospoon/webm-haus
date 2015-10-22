b19344f64de14729bd244a2dae5394df4a22b75f
Dima Dermanskyi <ocheyoba@gmail.com>
2015-10-22 21:28:11 +0300

    Jackson serialization of Video

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
 webm-haus
-<img src="https://travis-ci.org/thereisnospoon/webm-haus.svg?branch=master"/>
-=========
--- src/main/java/my/thereisnospoon/webm/entities/Video.java
+++ src/main/java/my/thereisnospoon/webm/entities/Video.java
@@ -1,10 +1,14 @@
 package my.thereisnospoon.webm.entities;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import org.immutables.value.Value;
 
 import java.time.LocalDate;
 
 @Value.Immutable
+@JsonSerialize(as = ImmutableVideo.class)
+@JsonDeserialize(as = ImmutableVideo.class)
 public abstract class Video {
 
 	public abstract String getId();
