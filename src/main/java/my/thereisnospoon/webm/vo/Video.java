2797de3ed46cccd7759760cc67744f334d131468
Dmytro Dermanskyi <Dmytro_Dermanskyi@epam.com>
2015-12-21 13:16:40 +0200

    Returned removed VOs

---
+++ src/main/java/my/thereisnospoon/webm/vo/Video.java
@@ -0,0 +1,20 @@
+package my.thereisnospoon.webm.vo;
+
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import org.immutables.value.Value;
+
+import java.time.LocalDate;
+
+@Value.Immutable
+@JsonSerialize(as = ImmutableVideo.class)
+@JsonDeserialize(as = ImmutableVideo.class)
+public interface Video {
+
+	String getId();
+	String getMd5Hash();
+	Integer getDuration();
+	String getThumbnailId();
+	Long getSize();
+	LocalDate getUploadDate();
+}
